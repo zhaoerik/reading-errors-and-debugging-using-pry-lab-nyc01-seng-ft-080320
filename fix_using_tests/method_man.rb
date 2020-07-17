6dd7dcc23d1c5b604a5075b96572ddd7ff791f38
Erik Zhao <erikzhao98@gmail.com>
2020-07-17 23:08:24 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -2,10 +2,17 @@
 require 'pry'
 def start_game(player1, player2)
   binding.pry
+<<<<<<< HEAD
   "Hello #{player1} & #{player2}"
 end
 
 def play_game(player1, player2)
+=======
+  "Hello #{player1} & #{playar2}"
+end
+
+def play_game(player2)
+>>>>>>> 79140d3fcc5c3120668375122c09e10a8cabe048
   binding.pry
   "#{player1} is better than #{player2}"
 end
--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -3,7 +3,11 @@ require 'pry'
 def snake_it_up(string)
   if string[0] == "s"
     binding.pry
+<<<<<<< HEAD
     "s"+ "s" + "s" + "s" + "s" + "s" + "s" + "s" + "s" + "s" + string
+=======
+    10 * "s" + string
+>>>>>>> 79140d3fcc5c3120668375122c09e10a8cabe048
   else
     string
   end
