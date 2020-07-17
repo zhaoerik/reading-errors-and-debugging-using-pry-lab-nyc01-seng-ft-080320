62a9cb4bfcc77dbfc4196fb657263410eeeb53c8
Erik Zhao <erikzhao98@gmail.com>
2020-07-17 23:05:40 +0000

    Done.

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -1,5 +1,5 @@
 # don't forget to add: require 'pry'
-
+require 'pry'
 def get_user_input
   gets.chomp
 end
@@ -11,9 +11,11 @@ def prompt_user
 end
 
 def selection(num)
-  if num = 1
+  if num == 1
+    binding.pry
      "YUM YUM MUNCH MUNCH MUNCH"
-  elsif num = 2
+  elsif num == 2
+    binding.pry
      "HAM HAM HAM IN MY TUMMY"
   end
 end
--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,8 +1,9 @@
 # don't forget to add: require 'pry'
-
+require 'pry'
 def snake_it_up(string)
   if string[0] == "s"
-    10 * "s" + string
+    binding.pry
+    "s"+ "s" + "s" + "s" + "s" + "s" + "s" + "s" + "s" + "s" + string
   else
     string
   end
